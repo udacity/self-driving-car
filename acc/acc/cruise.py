8a876ec26126bb893f4da45fb486271af9ff0af0
Ariel Nuñez <ingenieroariel@gmail.com>
2017-02-03 16:42:40 -0500

    Basic test suite for ACC micro challenge

---
+++ acc/acc/cruise.py
@@ -0,0 +1,10 @@
+
+def control(speed, acceleration, car_in_front, steer_torque):
+    """Adaptive Cruise Control
+    """
+    # --- Implement your solution here ---#
+    brake = 0
+    gas = 0
+    # ------------------------------------#
+
+    return brake, gas
